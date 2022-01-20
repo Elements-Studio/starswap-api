dcb4e1a6749dd5bf67f16857c7fb94e04e09acd1
chihuahuastrong <strongchihuahua@hotmail.com>
2022-01-20 12:46:28 +0800

    update test data

--- src/test/resources/barnard-test-data.sql
+++ src/test/resources/barnard-test-data.sql
@@ -155,7 +155,7 @@ VALUES
 UNIX_TIMESTAMP() * 1000,
 'admin',
 false,
-'FAI',
+'STAR',
 '',
 2,
 '0x4783d08fb16990bd35d83f3e23bf93b8',
@@ -584,3 +584,186 @@ UNIX_TIMESTAMP() * 1000,
 UNIX_TIMESTAMP() * 1000,
 'admin',
 0);
+
+
+-- -----------------------------------
+update token t set t.description = 'STAR' where t.token_id = 'STAR';
+
+-- 0x88e2677b89841cd4ee7c15535798e1c8::WEN::WEN
+INSERT INTO `starswap_barnard`.`token`
+(`token_id`,
+`created_at`,
+`created_by`,
+`deactived`,
+`description`,
+`icon_url`,
+`sequence_number`,
+`token_struct_address`,
+`token_struct_module`,
+`token_struct_name`,
+`updated_at`,
+`updated_by`,
+`version`,
+`scaling_factor`)
+VALUES
+('WEN',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+false,
+'WEN',
+'',
+2,
+'0x88e2677b89841cd4ee7c15535798e1c8',
+'WEN',
+'WEN',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+0,
+null);
+
+-- 0x88e2677b89841cd4ee7c15535798e1c8::SHARE::SHARE
+INSERT INTO `starswap_barnard`.`token`
+(`token_id`,
+`created_at`,
+`created_by`,
+`deactived`,
+`description`,
+`icon_url`,
+`sequence_number`,
+`token_struct_address`,
+`token_struct_module`,
+`token_struct_name`,
+`updated_at`,
+`updated_by`,
+`version`,
+`scaling_factor`)
+VALUES
+('SHARE',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+false,
+'SHARE',
+'',
+2,
+'0x88e2677b89841cd4ee7c15535798e1c8',
+'SHARE',
+'SHARE',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+0,
+null);
+
+
+-- STC - WEN
+
+INSERT INTO `starswap_barnard`.`liquidity_token`
+(`liquidity_token_address`,
+`token_x_id`,
+`token_y_id`,
+`created_at`,
+`created_by`,
+`deactived`,
+`description`,
+`sequence_number`,
+`updated_at`,
+`updated_by`,
+`version`)
+VALUES
+('0x4783d08fb16990bd35d83f3e23bf93b8',
+'STC',
+'WEN',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+false,
+'STC / WEN',
+1,
+UNIX_TIMESTAMP() * 1000,
+'admin',
+0);
+
+INSERT INTO `starswap_barnard`.`liquidity_pool`
+(`liquidity_token_address`,
+`token_x_id`,
+`token_y_id`,
+`pool_address`,
+`created_at`,
+`created_by`,
+`deactived`,
+`description`,
+`sequence_number`,
+`total_liquidity`,
+`updated_at`,
+`updated_by`,
+`version`)
+VALUES
+('0x4783d08fb16990bd35d83f3e23bf93b8',
+'STC',
+'WEN',
+'0x4783d08fb16990bd35d83f3e23bf93b8',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+false,
+'STC / WEN',
+1,
+0,
+UNIX_TIMESTAMP() * 1000,
+'admin',
+0);
+
+
+-- SHARE-STC
+
+INSERT INTO `starswap_barnard`.`liquidity_token`
+(`liquidity_token_address`,
+`token_x_id`,
+`token_y_id`,
+`created_at`,
+`created_by`,
+`deactived`,
+`description`,
+`sequence_number`,
+`updated_at`,
+`updated_by`,
+`version`)
+VALUES
+('0x4783d08fb16990bd35d83f3e23bf93b8',
+'SHARE',
+'STC',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+false,
+'SHARE / STC',
+1,
+UNIX_TIMESTAMP() * 1000,
+'admin',
+0);
+
+INSERT INTO `starswap_barnard`.`liquidity_pool`
+(`liquidity_token_address`,
+`token_x_id`,
+`token_y_id`,
+`pool_address`,
+`created_at`,
+`created_by`,
+`deactived`,
+`description`,
+`sequence_number`,
+`total_liquidity`,
+`updated_at`,
+`updated_by`,
+`version`)
+VALUES
+('0x4783d08fb16990bd35d83f3e23bf93b8',
+'SHARE',
+'STC',
+'0x4783d08fb16990bd35d83f3e23bf93b8',
+UNIX_TIMESTAMP() * 1000,
+'admin',
+false,
+'SHARE / STC',
+1,
+0,
+UNIX_TIMESTAMP() * 1000,
+'admin',
+0);
+
